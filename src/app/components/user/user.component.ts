4e737434e585469eedc775046a40189d69eaaff7
duongdinh24 <dvdinh24@gmail.com>
2024-09-28 15:22:57 +0700

    [SECTION-2][57] content projection

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -8,6 +8,7 @@ import {
   output,
 } from '@angular/core';
 import { User } from '../../models/user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 // type User = {
@@ -19,7 +20,7 @@ import { User } from '../../models/user.model';
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
